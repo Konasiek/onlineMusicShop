8c732106edca3dfde751ce52f107f4c90792cd81
Konasiek <konasiek@gmail.com>
2020-12-16 14:39:46 +0100

    adding Role, User repository

---
+++ src/main/java/com/online/musicshop/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.online.musicshop.repository;
+
+import com.online.musicshop.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
+}
