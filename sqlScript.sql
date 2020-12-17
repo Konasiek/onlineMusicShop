785014a2550b2220cf12d7d7ea2da29058f955dd
Konasiek <konasiek@gmail.com>
2020-12-17 12:49:04 +0100

    adding bootsrap, manual testing app

--- sqlScript.sql
+++ sqlScript.sql
@@ -1,2 +1,13 @@
 /*SQL script for onlineMusicShop*/
 CREATE SCHEMA `ms`;
+/*ROLE_USER has to be added to db to create new users*/
+INSERT INTO roles values (131, "ROLE_USER");
+
+INSERT INTO users values (111, "user12@gmail.com", "password", "useruser");
+SELECT * FROM users;
+
+SELECT * FROM roles;
+SELECT * FROM user_roles;
+
+DROP table roles, users, user_roles;
+
