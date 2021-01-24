a765398662b3b3431a1f862f6da7e0ccf058ea9e
Konasiek <konasiek@gmail.com>
2021-01-25 00:38:39 +0100

    adding cart icon with number of items in cart on it

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,53 +1,58 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../_services/auth.service';
 import {TokenStorageService} from '../_services/token-storage.service';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  form: any = {
-    username: null,
-    password: null
-  };
-  isLoggedIn = false;
-  isLoginFailed = false;
-  errorMessage = '';
-  roles: string[] = [];
+    form: any = {
+        username: null,
+        password: null
+    };
+    isLoggedIn = false;
+    isLoginFailed = false;
+    errorMessage = '';
+    roles: string[] = [];
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) {
-  }
-
-  ngOnInit(): void {
-    if (this.tokenStorage.getToken()) {
-      this.isLoggedIn = true;
-      this.roles = this.tokenStorage.getUser().roles;
+    constructor(private authService: AuthService,
+                private tokenStorage: TokenStorageService) {
     }
-  }
 
-  onSubmit(): void {
-    const {username, password} = this.form;
+    ngOnInit(): void {
+        if (this.tokenStorage.getToken()) {
+            this.isLoggedIn = true;
+            this.roles = this.tokenStorage.getUser().roles;
+        }
+    }
 
-    this.authService.login(username, password).subscribe(
-      data => {
-        this.tokenStorage.saveToken(data.accessToken);
-        this.tokenStorage.saveUser(data);
+    onSubmit(): void {
+        const {username, password} = this.form;
 
-        this.isLoginFailed = false;
-        this.isLoggedIn = true;
-        this.roles = this.tokenStorage.getUser().roles;
-        this.reloadPage();
-      },
-      err => {
-        this.errorMessage = err.error.message;
-        this.isLoginFailed = true;
-      }
-    );
-  }
+        this.authService.login(username, password).subscribe(
+            data => {
+                this.tokenStorage.saveToken(data.accessToken);
+                this.tokenStorage.saveUser(data);
 
-  reloadPage(): void {
-    window.location.reload();
-  }
+                this.isLoginFailed = false;
+                this.isLoggedIn = true;
+                this.roles = this.tokenStorage.getUser().roles;
+                this.reloadPage();
+            },
+            err => {
+                this.errorMessage = err.error.message;
+                this.isLoginFailed = true;
+            }
+        );
+    }
+
+    reloadPage(): void {
+
+        // this.router.navigate(['/product']);
+        window.location.reload();
+
+    }
 }
