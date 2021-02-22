772e9df91a66e8ab219814a32ef348c3794c53d2
Konasiek <konasiek@gmail.com>
2021-02-22 21:34:24 +0100

    adding Procfile

--- backend/src/main/java/com/online/musicshop/security/WebSecurityConfig.java
+++ backend/src/main/java/com/online/musicshop/security/WebSecurityConfig.java
@@ -70,9 +70,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				// to change data
-			.authorizeRequests().antMatchers("/api/auth/**", "/api/order**").permitAll()
+			.authorizeRequests().antMatchers("/api/auth/**", "/api/order**", "/**").permitAll()
 				// to read data
-			.antMatchers("/api/test/**", "/api/product**", "/api/order**", "/product**", "**").permitAll()
+			.antMatchers("/api/test/**", "/api/product**", "/api/order**", "/product**", "/**").permitAll()
 			.anyRequest().authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
