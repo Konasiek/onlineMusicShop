48d58f5a2ada181aeaadcec4b42159730d50db44
Konasiek <konasiek@gmail.com>
2021-03-03 12:13:51 +0100

    adding CategoryTest

--- backend/src/test/java/com/online/musicshop/models/CartTest.java
+++ backend/src/test/java/com/online/musicshop/models/CartTest.java
@@ -1,6 +1,5 @@
 package com.online.musicshop.models;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
---
+++ backend/src/test/java/com/online/musicshop/models/CategoryTest.java
@@ -0,0 +1,30 @@
+package com.online.musicshop.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = new Category();
+        category.setId(1001L);
+        category.setName("Bass");
+    }
+
+    @Test
+    void categoryTest() {
+
+        assertAll("Category tests",
+                () -> assertAll("Category properties",
+                        () -> assertEquals(1001L, (long) category.getId(), "Id failed"),
+                        () -> assertNotEquals(1002L, (long) category.getId(), "Id failed"),
+                        () -> assertEquals("Bass", category.getName(), "name failed"),
+                        () -> assertNotEquals("Bas", category.getName(), "name failed"))
+        );
+    }
+}
