0b6929d82a885cfff6227942e0df167b045028cc
Konasiek <konasiek@gmail.com>
2021-03-03 11:47:54 +0100

    adding CartTest

---
+++ backend/src/test/java/com/online/musicshop/models/CartTest.java
@@ -0,0 +1,37 @@
+package com.online.musicshop.models;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    Cart cart;
+    List<ProductInOrder> productsInOrder;
+
+    @BeforeEach
+    void setUp() {
+        cart = new Cart();
+        ProductInOrder productInOrder1 = new ProductInOrder();
+        productsInOrder = new ArrayList<>();
+        productsInOrder.add(productInOrder1);
+        cart.setId(10001L);
+        cart.setProductsInOrder(productsInOrder);
+    }
+
+    @Test
+    void cartTest() {
+        assertAll("Properties tests",
+                () -> assertAll("Cart properties",
+                        () -> assertEquals(10001L, (long) cart.getId(), "Id failed")),
+                () -> assertAll("Products in order tests",
+                        () -> assertFalse(cart.getProductsInOrder().isEmpty(), "Product in order failed"),
+                        () -> assertThrows(IndexOutOfBoundsException.class, () -> cart.getProductsInOrder().get(10)))
+        );
+    }
+}
